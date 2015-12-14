47b250f34212e9169570aed6df6560d33178b7c2
David Black <dtinblack@gmail.com>
2015-12-14 10:32:29 +0000

    ArgMax added to Mathematics

--- Mathematics/src/main/scala/Misc.scala
+++ Mathematics/src/main/scala/Misc.scala
@@ -12,10 +12,16 @@ package object Miscellaneous {
 
    def argMax[A, B: Ordering](input: Iterable[A], f: A => B) = {
 
+       // map 'input' by the function 'f'
+
        val fList = input map f
-       
+
+       // find the max value
+
        val maxFList = fList.max
 
+       // To access a zipped list x can use x._1 and x._2
+      
        input.view zip fList filter(_._2 == maxFList ) map (_._1) toSet
 
    }
