7de12e684eab02a849537d9d702d92faeea230ce
dtinblack <dtinblack@gmail.com>
2016-12-15 19:22:37 +0000

    added OwnType folder

--- TypeSystem/OwnTypes/OwnTypes.scala
+++ TypeSystem/OwnTypes/OwnTypes.scala
@@ -2,7 +2,8 @@
 
 object OwnTypes {
 
-    //========================================================
+    // =======================================================
+    // Example using shoe boxes in a cupboard
 
     trait Box {
        def full: Boolean
@@ -23,23 +24,57 @@ object OwnTypes {
 
     def get(b: ShoeBox): Int = b match { case ShoeBox(n) => n }
 
+    // =======================================================
+    // Example using class, apply() and unapply()
+
+    class Person(val name: String )
+
+    object Person{
+
+       def apply(name: String) = new Person( name )
+
+       // default
+
+       def apply() = new Person("No name")
+
+       // first and second name
+
+       def apply(first: String, second: String) = new Person( first + " " + second )
+
+       // Extractor - used in pattern matching
+
+       def unapply(p: Person):Option[String] = if ( p.name.contains("Hello") ) Some("Found Hello")
+                                                 else None
+
+    }
+
     def main(args: Array[String]){
 
       // How full is the shoe box ?
 
-      println("Is the shoebox full ? " + ShoeBox(20).full)
+        println("Is the shoebox full ? " + ShoeBox(20).full)
 
       // Fill the wardrobe up with shoe boxes
 
-      val wardrobe = CupBoard(List(ShoeBox(12), ShoeBox(10), ShoeBox(5)))
+        val wardrobe = CupBoard(List(ShoeBox(12), ShoeBox(10), ShoeBox(5)))
 
       // How many shoe boxes in the cupboard ?
 
-      println("Number of shoe boxes in the wardrobe: " + numberOfBoxes( wardrobe.contents ))
+        println("Number of shoe boxes in the wardrobe: " + numberOfBoxes( wardrobe.contents ))
 
       // Total number of items in the cupboard ?
 
-      println("Total number of items in the cupboard: " + wardrobe.contents.map(get).foldLeft(0)((r,c) => r +c ))
+        println("Total number of items in the cupboard: " + wardrobe.contents.map(get).foldLeft(0)((r,c) => r +c ))
+
+      // What is the name of the Person ?
+
+        println("The name of the person is: " + Person("Hello", "World").name )
+
+
+        Person("Hello World") match {
+                case Person(s) => println(s)
+                case _ => println("Error")
+        }
 
 
     } // End of main
